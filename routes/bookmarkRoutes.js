641551e0e9f404e5c5648cfabc0a683d8d97a3a0
AtiqurRahman895 <emonhassan895@gmail.com>
2025-04-21 20:35:25 +0600

    ADDED get routes to load all bookmarked  questions, blogs and check if the question or blog is already bookmarked.

--- routes/bookmarkRoutes.js
+++ routes/bookmarkRoutes.js
@@ -23,7 +23,54 @@ router.post("/addToBookmark", verifyToken, isUserOnDB, async (req, res) => {
   }
 });
 
+router.get("/bookmarks", verifyToken, isUserOnDB, async (req, res) => {
 
+    try {    
+    const {email} = req.headers
+    const bookmarksList = await bookmarks.find({email}).toArray();
+  
+      // Separate question & blog item ObjectIds
+      const questionIds = bookmarksList
+        .filter(b => b.type === "question")
+        .map(b => new ObjectId(b.item) );
+  
+      const blogIds = bookmarksList
+        .filter(b => b.type === "blog")
+        .map(b => new ObjectId(b.item));
+  
+      // Fetch actual data
+      const [questionsList,blogsList] = await Promise.all([
+        questionIds.length
+          ? questions.find({ _id: { $in: questionIds } }).toArray()
+          : [],
+        blogIds.length
+          ? blogs.find({ _id: { $in: blogIds } }).toArray()
+          : []
+      ]);
+  
+        res.json({
+            questions: questionsList || [],
+            blogs: blogsList || []
+        });
+    
+      } catch (error) {
+        console.error(`Failed to find bookmark items: ${error}`);
+        res.status(500).send("Failed to find bookmark items.");
+      }
+});
+
+router.get("/bookmark/:item", verifyToken, isUserOnDB, async (req, res) => {
+    const {email} = req.headers
+    let item = req.params.item;
+
+    try {
+        const result = await bookmarks.findOne({email, item})
+        res.status(200).json(result)
+    } catch (error) {
+        console.error(`Failed to find bookmark item: ${error}`);
+        res.status(500).send("Failed to find bookmark item.");
+    }
+});
 
 router.delete("/deleteFromBookmark/:_id", verifyToken, isUserOnDB, async (req, res) => {
     const {email} = req.headers
